fb4738e78633288a79e2111fdb677fc0ea2a07bc
Tóth Béla <lupa7240@gmail.com>
2024-11-07 18:29:30 +0100

    Add inital files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import AboutSection from './components/AboutSection';
+import LinkCard from './components/LinkCard';
+import { 
+  FaGithub, 
+  FaTwitch
+} from 'react-icons/fa';
+import {
+  SiKofi
+} from 'react-icons/si'
 import './App.css';
+import GameCard, { GameCardProps } from './components/GameCard';
+import gameData from './gamecard-list.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App: React.FC = () => {
+
+  const [gamedata, setGameData] = useState<GameCardProps[]>([])
+
+  useEffect(() => {
+    setGameData(gameData)
+  }, [])
+
+  return(
+    <main className="home-page" >
+      <AboutSection />
+      <div className="link-list">
+        <LinkCard title="My Ko-fi page" url="https://ko-fi.com/achiegamedev" description="Devlogs, Reviews and much more" icon={<SiKofi/>}/>
+        <LinkCard title="GitHub" url="https://github.com/your-profile" description="Check out my code projects" icon={<FaGithub/>}/>
+        <LinkCard title="LinkedIn" url="https://linkedin.com/in/your-profile" description="Connect with me" />
+        <LinkCard title="Portfolio" url="https://your-portfolio.com" description="See my latest work" />
+      </div>
+      <p></p>
+      <div className="gamecard-list">
+        {gamedata.map((game, key) =>
+          <GameCard key={key } {...game}/>
+        )}
+      </div>
+
+    </main>
+  )
 }
 
 export default App;
---
+++ src/components/AboutSection.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AboutSection: React.FC = () => (
+  <section className="about-section">
+    <h2>AchieGameDev</h2>
+    <p>Software Engineer by day, Indie game developer/content creator by night.</p>
+  </section>
+);
+
+export default AboutSection;
