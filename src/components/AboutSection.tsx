6efae72b66d69548df5d35cdd10c769638b48e86
Tóth Béla <lupa7240@gmail.com>
2024-11-07 21:01:50 +0100

    Add linter because I like pain

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const AboutSection: React.FC = () => (
   <section className="about-section">
     <h2>AchieGameDev</h2>
-    <p>Software Engineer by day, Indie game developer/content creator by night.</p>
+    <p>
+      Software Engineer by day, Indie game developer/content creator by night.
+    </p>
   </section>
 );
 
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,33 +1,39 @@
 import React from "react";
 
 export type GameCardProps = {
-    thumbnailPath: string,
-    title: string,
-    summary: string,
-    tools: string,
-    description: string,
-    altText: string
-}
+  thumbnailPath: string;
+  title: string;
+  summary: string;
+  tools: string;
+  description: string;
+  altText: string;
+};
 
 const GameCard: React.FC<GameCardProps> = ({
-    thumbnailPath,
-    title,
-    summary,
-    tools,
-    description,
-    altText
+  thumbnailPath,
+  title,
+  summary,
+  tools,
+  description,
+  altText,
 }) => (
-    <div className="game-card-container">
-        <div className="game-card-thumbnail-container">
-            <img src={thumbnailPath}></img>
-        </div>
-        <div className="game-card-text-container">
-            <p><strong>Title:</strong> {title}</p>
-            <p><strong>Summary:</strong> {summary}</p>
-            <p><strong>Tools:</strong> {tools}</p>
-            <p> {description}</p>
-        </div>
+  <div className="game-card-container">
+    <div className="game-card-thumbnail-container">
+      <img src={thumbnailPath} alt={altText}></img>
     </div>
+    <div className="game-card-text-container">
+      <p>
+        <strong>Title:</strong> {title}
+      </p>
+      <p>
+        <strong>Summary:</strong> {summary}
+      </p>
+      <p>
+        <strong>Tools:</strong> {tools}
+      </p>
+      <p> {description}</p>
+    </div>
+  </div>
 );
 
-export default GameCard
+export default GameCard;
